1b937cb9a1f32a113ac2675d9d90c20688d2ae2a
Andy Matuschak <andy@khanacademy.org>
2014-10-03 17:45:22 -0700

    Made framework target; empty Layer class

---
+++ Prototope/Layer.swift
@@ -0,0 +1,13 @@
+//
+//  Layer.swift
+//  Prototope
+//
+//  Created by Andy Matuschak on 10/3/14.
+//  Copyright (c) 2014 Khan Academy. All rights reserved.
+//
+
+import UIKit
+
+public class Layer: UIView {
+	
+}
--- PrototopeTestApp/ViewController.swift
+++ PrototopeTestApp/ViewController.swift
@@ -7,11 +7,13 @@
 //
 
 import UIKit
+import Prototope
 
 class ViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		Layer()
 		// Do any additional setup after loading the view, typically from a nib.
 	}
 
