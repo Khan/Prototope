011c1ac56dddfb1a256379c34ac214e348813b46
Andy Matuschak <andy@khanacademy.org>
2014-10-16 15:05:05 -0700

    map()

--- Prototope/Math.swift
+++ Prototope/Math.swift
@@ -12,3 +12,8 @@ import Foundation
 public func interpolate(from fromValue: Double, to toValue: Double, at fraction: Double) -> Double {
 	return fraction * (toValue - fromValue) + fromValue
 }
+
+/// Maps a value from one interval to another.
+public func map(#value: Double, #fromInterval: (Double, Double), #toInterval: (Double, Double)) -> Double {
+	return interpolate(from: toInterval.0, to: toInterval.1, at: (value - fromInterval.0) / (fromInterval.1 - fromInterval.0))
+}
---
+++ PrototopeTests/MathTests.swift
@@ -0,0 +1,22 @@
+//
+//  MathTests.swift
+//  Prototope
+//
+//  Created by Andy Matuschak on 10/16/14.
+//  Copyright (c) 2014 Khan Academy. All rights reserved.
+//
+
+import Prototope
+import XCTest
+import Foundation
+
+class MathTests: XCTestCase {
+	func testInterpolate() {
+		XCTAssertEqual(interpolate(from: 3, to: 9, at: 0.25), 4.5)
+		XCTAssertEqual(interpolate(from: 10, to: 4, at: 0.5), 7)
+	}
+
+	func testMap() {
+		XCTAssertEqual(map(value: 4, fromInterval: (2, 8), toInterval: (1, 4)), 2)
+	}
+}
