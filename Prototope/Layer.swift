021639449d3971f7b3980f5ddd5e9cb834527e37
Andy Matuschak <andy@khanacademy.org>
2014-12-03 13:03:06 -0500

    Added more detailed conceptual Gesture documentation.

--- Prototope/Layer.swift
+++ Prototope/Layer.swift
@@ -346,6 +346,16 @@ public class Layer: Equatable, Hashable {
 	// MARK: Touches and gestures
 
 	/** An array of the layer's gestures. Append a gesture to this list to add it to the layer.
+	
+		Gestures are like a higher-level abstraction than the Layer touch handler API. For
+		instance, a pan gesture consumes a series of touch events but does not actually begin
+		until the user moves a certain distance with a specified number of fingers.
+	
+		Gestures can also be exclusive: by default, if a gesture recognizes, traditional
+		touch handlers for that subtree will be cancelled. You can control this with the
+		cancelsTouchesInView property. Also by default, if one gesture recognizes, it will
+		prevent all other gestures involved in that touch from recognizing.
+	
 		Defaults to the empty list. */
 	public var gestures: [GestureType] = [] {
 		didSet {
