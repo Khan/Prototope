885a2001e0f68a7c9c314f20fc7ec8e28357dba3
Andy Matuschak <andy@khanacademy.org>
2014-12-02 18:44:33 -0800

    Documented Math.

--- Prototope/Math.swift
+++ Prototope/Math.swift
@@ -8,16 +8,28 @@
 
 import Foundation
 
-/// Like Processing's lerp()
+/** Linearly interpolates between the from: value and the to: value based on the at:
+	fraction. When at:0, returns the from: value. When at: 1, returns the to: value.
+
+	For example:
+		interpolate(from: 5, to: 15, at: 0.5) // Returns 10
+
+	Like Processing's lerp() */
 public func interpolate(from fromValue: Double, to toValue: Double, at fraction: Double) -> Double {
 	return fraction * (toValue - fromValue) + fromValue
 }
 
-/// Maps a value from one interval to another.
+/** Maps a value from one interval to another.
+
+	For example:
+		map(value: 0.4, fromInterval: (0, 1), toInterval: (0, 10)) // Returns 4
+
+	Like Processing's map(). */
 public func map(value: Double, #fromInterval: (Double, Double), #toInterval: (Double, Double)) -> Double {
 	return interpolate(from: toInterval.0, to: toInterval.1, at: (value - fromInterval.0) / (fromInterval.1 - fromInterval.0))
 }
 
+/** Clips a value so that it falls between the specified minimum and maximum. */
 public func clip<T: Comparable>(value: T, min minValue: T, max maxValue: T) -> T {
 	return max(min(value, maxValue), minValue)
 }
