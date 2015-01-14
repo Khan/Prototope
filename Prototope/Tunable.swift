fdee8ffae5e2ca4b2196662749d65e252220cd64
Andy Matuschak <andy@khanacademy.org>
2015-01-14 10:18:43 -0800

    Added higher-level docs on tunable()

--- Prototope/Tunable.swift
+++ Prototope/Tunable.swift
@@ -8,6 +8,33 @@
 
 import Foundation
 
+/* These functions allow you to take a value and make it tunable by a slider or a switch!
+
+	Example usage:
+		tunable(0.5, name: "button alpha") { alpha in someButton.alpha = alpha }
+	Whenever the alpha changes, the closure will be run, and the button's alpha will be updated.
+
+	If an instantaneous return value is good enough, you can do this instead:
+		someButton.alpha = tunable(0.5, name: "button alpha")
+
+	Once you've tuned values in the UI, you can apply them back to the project by following these steps:
+	1. Tap "Share"
+
+	2a. If you're attached to Xcode, open up the console (View > Debug Area > Activate Console) and look for a log like this:
+		[
+			{
+			"label" : "Test",
+			"key" : "Test",
+			"sliderMinValue" : 0,
+			"sliderMaxValue" : 1400,
+			"sliderValue" : 179.6053
+			}
+		].
+
+	2b. If you're not attached to Xcode, email yourself from the share sheet.
+
+	3. Copy and paste the contents into TunableSpec.json in your project (included with OhaiPrototope). */
+
 /** Returns the tunable value stored in TunableValues.json under `name`. If none is present, returns `defaultValue`. */
 public func tunable(defaultValue: Double, #name: String) -> Double {
 	if let specItem = defaultSpec._KFSpecItemForKey(name) {
