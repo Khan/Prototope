44c164ede820b767ae36079b7830d16a0f10efb6
Andy Matuschak <andy@khanacademy.org>
2015-02-02 16:50:55 -0800

    Bridged size

--- PrototopeJSBridge/GeometryBridge.swift
+++ PrototopeJSBridge/GeometryBridge.swift
@@ -14,6 +14,7 @@ import Prototope
 	// TODO make these writable, add observers to the bridges which vend them
 	var x: Double { get }
 	var y: Double { get }
+	class var zero: PointJSExport // exported manually
 	init(args: NSDictionary)
 	func distanceToPoint(point: PointJSExport) -> Double
 	var length: Double { get }
@@ -36,7 +37,7 @@ import Prototope
 	public required init(args: NSDictionary) {
 		point = Point(
 			x: (args["x"] as Double?) ?? 0,
-			y: (args["x"] as Double?) ?? 0
+			y: (args["y"] as Double?) ?? 0
 		)
 		super.init()
 	}
@@ -73,5 +74,54 @@ import Prototope
 
 	public func divide(scalar: Double) -> PointJSExport {
 		return PointBridge(point / scalar)
+	}
+}
+
+
+@objc public protocol SizeJSExport: JSExport {
+	var width: Double { get }
+	var height: Double { get }
+	class var zero: SizeJSExport // exported manually
+	init(args: NSDictionary)
+
+	func equals(other: SizeJSExport) -> Bool
+	func add(other: SizeJSExport) -> SizeJSExport
+	func multiply(scalar: Double) -> SizeJSExport
+}
+
+@objc public class SizeBridge: NSObject, SizeJSExport, BridgeType {
+	public class func addToContext(context: JSContext) {
+		context.setObject(self, forKeyedSubscript: "Size")
+		let sizeBridge = context.objectForKeyedSubscript("Size")
+		sizeBridge.setObject(self(args: ["width": 0, "height": 0]), forKeyedSubscript: "zero")
+	}
+
+	var size: Size
+	public required init(args: NSDictionary) {
+		size = Size(
+			width: (args["width"] as Double?) ?? 0,
+			height: (args["height"] as Double?) ?? 0
+		)
+		super.init()
+	}
+
+	init(_ size: Size) {
+		self.size = size
+		super.init()
+	}
+
+	public var width: Double { return size.width }
+	public var height: Double { return size.height }
+
+	public func equals(other: SizeJSExport) -> Bool {
+		return size == (other as JSExport as SizeBridge).size
+	}
+
+	public func add(other: SizeJSExport) -> SizeJSExport {
+		return SizeBridge(size + (other as JSExport as SizeBridge).size)
+	}
+
+	public func multiply(scalar: Double) -> SizeJSExport {
+		return SizeBridge(size * scalar)
 	}
 }
