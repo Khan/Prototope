7a40120729cff766a1ab481edb032d1c0361fcc9
Andy Matuschak <andy@khanacademy.org>
2015-02-06 14:10:33 -0800

    Basic target structure for Protocaster

---
+++ Protocaster/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Protocaster
+//
+//  Created by Andy Matuschak on 2/6/15.
+//  Copyright (c) 2015 Khan Academy. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ Protocaster/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Protocaster
+//
+//  Created by Andy Matuschak on 2/6/15.
+//  Copyright (c) 2015 Khan Academy. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: AnyObject? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
