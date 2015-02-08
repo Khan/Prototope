2f4fe0336215fa0e23c98e0cac39f0ec1044e234
Saniul Ahmed <saniul.ahmed@gmail.com>
2015-02-07 23:00:51 -0800

    working collision constraints

--- Prototope/Collisions.swift
+++ Prototope/Collisions.swift
@@ -8,6 +8,73 @@
 
 import Foundation
 
+var globalConstraintStore = ConstraintStore()
+
+class ConstraintStore {
+
+    let heartbeat: Heartbeat!
+
+    var registeredConstraints: [Constraint] {
+        didSet {
+            self.heartbeat.paused = self.registeredConstraints.count == 0
+        }
+    }
+
+    init() {
+        self.registeredConstraints = [Constraint]()
+        self.heartbeat = Heartbeat { _ in
+            self.tick()
+        }
+    }
+
+    func tick() {
+        for c in self.registeredConstraints {
+            c.update()
+        }
+    }
+
+    func registerConstraint(constraint: Constraint) {
+        self.registeredConstraints.append(constraint)
+    }
+
+    func unregisterConstraint(constraint: Constraint) {
+        if let idx = find(self.registeredConstraints, constraint) {
+            self.registeredConstraints.removeAtIndex(idx)
+        }
+    }
+}
+
+public func when(l: Layer) -> ConstraintLHS {
+    return ConstraintLHS(layer1: l)
+}
+
+public struct ConstraintLHS {
+    public typealias Handler = () -> Void
+    
+    let layer1: Layer
+    
+    public func collidesWith(layer2: Layer, handler: Handler) -> Constraint {
+        return Constraint(layer1: layer1, layer2: layer2, kind: .Collides, handler: handler)
+    }
+    
+    public func enters(layer2: Layer, handler: Handler) -> Constraint {
+        return Constraint(layer1: layer1, layer2: layer2, kind: .Enters, handler: handler)
+    }
+    
+    public func leaves(layer2: Layer, handler: Handler) -> Constraint {
+        return Constraint(layer1: layer1, layer2: layer2, kind: .Leaves, handler: handler)
+    }
+    
+    public func isContainedIn(layer2: Layer, handler: Handler) -> Constraint {
+        return Constraint(layer1: layer1, layer2: layer2, kind: .IsContainedIn, handler: handler)
+    }
+
+    public func contains(layer2: Layer, handler: Handler) -> Constraint {
+        return Constraint(layer1: layer1, layer2: layer2, kind: .Contains, handler: handler)
+    }
+}
+
+
 enum ConstraintKind {
     case Collides
     
@@ -25,12 +92,53 @@ enum ConstraintState {
     case Contains
 }
 
-struct Constraint {
-    let kind: ConstraintKind
+func stateForLayer(layer1: Layer, andLayer layer2: Layer) -> ConstraintState {
+    let rect1 = CGRect(layer1.frame)
+    let rect2 = CGRect(layer2.frame)
     
+    if !CGRectIntersectsRect(rect1, rect2) {
+        return .NonOverlapping
+    }
+    
+    if CGRectContainsRect(rect1, rect2) {
+        return .Contains
+    } else if CGRectContainsRect(rect2, rect1) {
+        return .ContainedIn
+    }
+    
+    return .PartiallyIntersects
+}
+
+public func ==(c1: Constraint, c2: Constraint) -> Bool {
+    return c1.id == c2.id
+}
+
+
+var constraintCounter = 0
+public class Constraint : Equatable {
+
+    let id: Int
+    let layer1: Layer
+    let layer2: Layer
+    let kind: ConstraintKind
     var previousState: ConstraintState
     
     let handler: () -> Void
+    
+    init(layer1: Layer, layer2: Layer, kind: ConstraintKind, handler: () -> Void) {
+        self.id = constraintCounter++
+        self.layer1 = layer1
+        self.layer2 = layer2
+        self.kind = kind
+        self.previousState = stateForLayer(layer1, andLayer: layer2)
+        self.handler = handler
+
+        globalConstraintStore.registerConstraint(self)
+    }
+
+    func update() {
+        self.updateWithState(stateForLayer(layer1, andLayer: layer2))
+    }
     
     func updateWithState(state: ConstraintState) {
         let old = previousState
@@ -65,9 +173,12 @@ struct Constraint {
         default:
             ()
         }
+        
+        self.previousState = state
     }
     
     func fire() {
         handler ()
     }
+
 }
