2e40c7bdb460667bca95fe73e2ee0e3f3d9b8975
Andy Matuschak <andy@khanacademy.org>
2015-02-09 12:57:08 -0800

    Implemented Message and Prototype encode/decoding

--- Protorope/Message.swift
+++ Protorope/Message.swift
@@ -7,7 +7,44 @@
 //
 
 import Foundation
+import swiftz
+import swiftz_core
 
 enum Message {
 	case ReplacePrototype(Prototype)
+}
+
+extension Message: JSONDecode {
+	static func fromJSON(jsonValue: JSONValue) -> Message? {
+		switch jsonValue {
+		case let .JSONObject(dictionary):
+			return dictionary["type"]
+				>>- MessageTypeEncoding.fromJSON
+				>>- { type in
+					dictionary["payload"]
+					>>- self.decodeMessageType(type)
+				}
+		default:
+			return nil
+		}
+	}
+
+	private static func decodeMessageType(type: MessageTypeEncoding)(payload: JSONValue) -> Message? {
+		switch type {
+		case .ReplacePrototype:
+			return Prototype.fromJSON(payload) >>- { .ReplacePrototype($0) }
+		}
+	}
+
+	enum MessageTypeEncoding: String, JSON {
+		case ReplacePrototype = "ReplacePrototype"
+
+		static func fromJSON(jsonValue: JSONValue) -> MessageTypeEncoding? {
+			return JString.fromJSON(jsonValue) >>- { MessageTypeEncoding(rawValue: $0) }
+		}
+
+		static func toJSON(jsonValue: MessageTypeEncoding) -> JSONValue {
+			return JString.toJSON(jsonValue.rawValue)
+		}
+	}
 }
--- Prototype.swift
+++ Prototype.swift
@@ -7,8 +7,42 @@
 //
 
 import Foundation
+import swiftz
+import swiftz_core
 
 struct Prototype {
 	var mainScript: NSData
-	var images: [String -> NSData]
+	var images: [String: NSData]
+}
+
+extension Prototype: JSON {
+	private static func create(mainScript: NSData)(images: [String: NSData]) -> Prototype { return Prototype(mainScript: mainScript, images: images) }
+
+	static func fromJSON(jsonValue: JSONValue) -> Prototype? {
+		switch jsonValue {
+		case let .JSONObject(dictionary):
+			return create
+				<^> (dictionary["mainScript"] >>- NSDataJSONCoder.fromJSON)
+				<*> (dictionary["images"] >>- JDictionaryFrom<NSData, NSDataJSONCoder>.fromJSON)
+		default:
+			return nil
+		}
+	}
+
+	static func toJSON(prototype: Prototype) -> JSONValue {
+		return .JSONObject([
+			"mainScript": NSDataJSONCoder.toJSON(prototype.mainScript),
+			"images": JDictionaryTo<NSData, NSDataJSONCoder>.toJSON(prototype.images)
+		])
+	}
+}
+
+struct NSDataJSONCoder: JSON {
+	static func fromJSON(jsonValue: JSONValue) -> NSData? {
+		return JString.fromJSON(jsonValue) >>- { NSData(base64EncodedString: $0, options: nil) }
+	}
+
+	static func toJSON(data: NSData) -> JSONValue {
+		return JString.toJSON(data.base64EncodedStringWithOptions(nil))
+	}
 }
