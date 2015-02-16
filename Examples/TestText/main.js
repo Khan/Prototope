c858d490a4f7e3d2a7c24e254a02664a06c14e47
Andy Matuschak <andy@khanacademy.org>
2015-02-16 12:21:03 -0800

    Added TextLayer test example

---
+++ Examples/TestText/main.js
@@ -0,0 +1,24 @@
+Layer.root.backgroundColor = new Color({hue: 0.5, saturation: 0.9, brightness: 0.4})
+
+var singleLineLayer = new TextLayer()
+singleLineLayer.text = "This is a single centered line"
+singleLineLayer.border = new Border({color: Color.red, width: 1})
+singleLineLayer.textColor = new Color({white: 0.85})
+singleLineLayer.fontName = "Optima"
+singleLineLayer.fontSize = 25
+singleLineLayer.x = Layer.root.x
+singleLineLayer.y = 30
+
+
+var wrappingLayer = new TextLayer()
+wrappingLayer.text = "Hello there and welcome to Prototope, where we wrap text for you if you want."
+wrappingLayer.textColor = Color.white
+wrappingLayer.wraps = true
+wrappingLayer.border = new Border({color: Color.red, width: 1})
+wrappingLayer.x = Layer.root.x
+wrappingLayer.y = 300
+wrappingLayer.width = 100
+
+var h = new Heartbeat({handler: function(heartbeat) {
+	wrappingLayer.width = 100 + Math.sin(heartbeat.timestamp * 4) * 50
+}})
