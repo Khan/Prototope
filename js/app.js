517f40e3eed4a2555fb6a60056fdc9bc8407323b
Andy Matuschak <andy@khanacademy.org>
2015-03-11 20:40:52 -0400

    Added UI for choosing JS docs

--- js/app.js
+++ js/app.js
@@ -12,3 +12,36 @@ $("h4").each(function(i, e){
     })
 })
 */
+
+var Language = {
+	Swift: "Swift",
+	JavaScript: "JavaScript"
+}
+
+var languageKey = "language"
+var defaultLanguage = Language.JavaScript
+var currentLanguage = localStorage.getItem(languageKey) || defaultLanguage
+updateStylesForLanguage()
+
+function setLanguage(language) {
+	currentLanguage = language
+	localStorage.setItem(languageKey, language)
+	updateStylesForLanguage()
+}
+
+function updateStylesForLanguage() {
+	var classList = document.body.classList
+	switch (currentLanguage) {
+	case Language.Swift:
+		classList.remove("javascript")
+		classList.add("swift")
+		break
+	case Language.JavaScript:
+		classList.add("javascript")
+		classList.remove("swift")
+		break
+	default:
+		throw "Unknown language " + currentLanguage
+	}
+}
+
