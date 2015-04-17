4fe2f8f23fe43173c44fb8cf6e5a6488ba379396
Saniul Ahmed <saniul.ahmed@gmail.com>
2015-04-16 22:08:10 -0700

    Protocaster: Remember last selected path

--- Protocaster/ViewController.swift
+++ Protocaster/ViewController.swift
@@ -10,6 +10,8 @@ import Cocoa
 import swiftz_core
 
 let LastSelectedDeviceNameKey = "LastSelectedDeviceNameKey"
+let LastSelectedPathURLKey = "LastSelectedPathURLKey"
+
 class ViewController: NSViewController {
 
 	var selectedPathDidChange: (NSURL? -> ())?
@@ -26,13 +28,35 @@ class ViewController: NSViewController {
 			NSUserDefaults.standardUserDefaults().setObject(newValue, forKey: LastSelectedDeviceNameKey)
 		}
 	}
+    
+    var lastSelectedPathURL: NSURL? {
+        get {
+            return NSUserDefaults.standardUserDefaults().URLForKey(LastSelectedPathURLKey)
+        }
+        
+        set {
+            if let URL = newValue {
+                NSUserDefaults.standardUserDefaults().setURL(URL, forKey: LastSelectedPathURLKey)
+            } else {
+                NSUserDefaults.standardUserDefaults().setNilValueForKey(LastSelectedPathURLKey)
+            }
+        }
+    }
 
+    @IBOutlet weak var pathControl: NSPathControl!
 	@IBOutlet var deviceListController: NSArrayController!
 	@IBOutlet weak var deviceChooserButton: NSPopUpButton!
 	@IBOutlet weak var deviceSettingsCheckbox: NSButton!
+    
+    override func awakeFromNib() {
+        pathControl.URL = lastSelectedPathURL
+    }
 
 	@IBAction func pathControlDidChange(sender: NSPathControl) {
 		selectedPathDidChange?(sender.URL)
+        if let URL = sender.URL {
+            lastSelectedPathURL = URL
+        }
 	}
 
 	@IBAction func deviceSelectionDidChange(sender: NSPopUpButton) {
