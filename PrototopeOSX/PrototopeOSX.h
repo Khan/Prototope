9a2518e840c543a50fc47b63f3df6bb040bd7966
Jason Brennan <i.jasonbrennan@gmail.com>
2015-07-12 18:02:43 -0400

    Add PrototopeOSX framework target.

---
+++ PrototopeOSX/PrototopeOSX.h
@@ -0,0 +1,19 @@
+//
+//  PrototopeOSX.h
+//  PrototopeOSX
+//
+//  Created by Jason Brennan on 2015-07-12.
+//  Copyright (c) 2015 Khan Academy. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for PrototopeOSX.
+FOUNDATION_EXPORT double PrototopeOSXVersionNumber;
+
+//! Project version string for PrototopeOSX.
+FOUNDATION_EXPORT const unsigned char PrototopeOSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PrototopeOSX/PublicHeader.h>
+
+
