ebc558a049cdd9b9b0dc683d4d143270629e2d28
Jason Brennan <i.jasonbrennan@gmail.com>
2015-08-10 18:15:58 -0400

    Oh my god it builds
    It seems to crash, missing Core Audio? But at least it builds!

--- PrototopeOSX/PrototopeOSX.h
+++ PrototopeOSX/PrototopeOSX.h
@@ -17,3 +17,4 @@ FOUNDATION_EXPORT const unsigned char PrototopeOSXVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <PrototopeOSX/PublicHeader.h>
 
 
+//#import <pop/POP.h>
