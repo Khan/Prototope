1a08039407283f19fbd38e9bf675cd0c3795e04c
Jason Brennan <i.jasonbrennan@gmail.com>
2016-03-14 21:36:30 -0400

    Bridge Image(text:font:textColor:) to JS

--- PrototopeJSBridge/ImageBridge.swift
+++ PrototopeJSBridge/ImageBridge.swift
@@ -25,6 +25,18 @@ import JavaScriptCore
 		if let imageName = args["name"] as! String? {
 			image = Image(name: imageName)
 			super.init()
+		} else if let text = args["text"] as! String? {
+			
+			// TODO(jb): Replace this with a FontBridge type.
+			let fontName = (args["fontName"] as! String?) ?? "Helvetica"
+			let fontSize = (args["fontSize"] as! Double?) ?? 18
+			let font = SystemFont(name: fontName, size: CGFloat(fontSize))
+			
+			let textColor = (args["textColor"] as! ColorBridge).color ?? Color.black
+			
+			image = Image(text: text, font: font!, textColor: textColor)
+			
+			super.init()
 		} else {
 			super.init()
 			return nil
